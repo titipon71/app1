f152dd809fbb8faa53b5c685a6ebba83b08ac6ba
TITIPON KAOROBTHAM <s6703052411180@email.kmutnb.ac.th>
2025-12-18 11:57:59 +0700

    Update App.js whit css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <table border="1">
-      <tr><th>Product</th><th>Price</th></tr>
-      <tr><td>React</td><td>320</td></tr>
-      <tr><td>React Native</td><td>340</td></tr>
-    </table>
-  );
+  const divStyle = {
+    color: "red",
+    backgroundColor: "powderblue",
+    fontSize: "larger",
+    padding: "3px"
+  }
+  return <div style={divStyle}>Hello React</div>
+
+  // return (
+  //   <table border="1">
+  //     <tr><th>Product</th><th>Price</th></tr>
+  //     <tr><td>React</td><td>320</td></tr>
+  //     <tr><td>React Native</td><td>340</td></tr>
+  //   </table>
+  // );
 }
 
 export default App;
