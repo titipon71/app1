1b26b91a08522ba26e9b5138a0916757b564d690
Atom <s6703052411180@email.kmutnb.ac.th>
2025-12-25 09:45:23 +0700

    Add Css Externel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import "./style.css";
 
 function App() {
-  const divStyle = {
-    color: "red",
-    backgroundColor: "powderblue",
-    fontSize: "larger",
-    padding: "3px"
-  }
-  return <div style={divStyle}>Hello React</div>
+  // const divStyle = {
+  //   color: "red",
+  //   backgroundColor: "powderblue",
+  //   fontSize: "larger",
+  //   padding: "3px"
+  // }
+  return (
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className="title">React & React Native</div>
+    </>
+  )
 
   // return (
   //   <table border="1">
