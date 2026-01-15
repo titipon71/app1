3ee4cb8f3890af269d6d5e84843c9f2785cc329e
K.TITIPON <s6703052411180@email.kmutnb.ac.th>
2026-01-15 10:32:38 +0700

    Refs()

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
 import React from "react";
-import EventData2 from "./event-data";
-// import "./style.css";
-// import { Footer,Content, Header } from "./func-components";
-// import Calender from "./class-components";
-// import Button from "./class-components";
-function App() {
-    return <EventData2/>
+import RefsArray from "./refs-array";
 
-  // const divStyle = {
-  //   color: "red",
-  //   backgroundColor: "powderblue",
-  //   fontSize: "larger",
-  //   padding: "3px"
-  // }
-  // return (
-  //   <>
-  //   <Header/>
-  //   <p><center><Calender/></center></p>
-  //   <Content/><Footer/>
-  //   </>
-  // )
-  // (
-  //   <>
-  //   <h2>DeveloperThai.com</h2>
-  //   <div className="title">React & React Native</div>
-  //   </>
-  // )
-
-  // return (
-  //   <table border="1">
-  //     <tr><th>Product</th><th>Price</th></tr>
-  //     <tr><td>React</td><td>320</td></tr>
-  //     <tr><td>React Native</td><td>340</td></tr>
-  //   </table>
-  // );
+export default function App() {
+    return <RefsArray/>
 }
-
-export default App;
