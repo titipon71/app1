5a09de99435e5bb514da1022394baa45a270f9af
titipon71 <s6703052411180@email.kmuntb.ac.th>
2026-02-19 10:16:28 +0700

    Implement Router3, Home, Member, and Products components; update App to use Router3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Router2 from "./router2";
+import Router3 from "./router3";
 
 export default function App() {
-  return <Router2 />;
+  return <Router3 />;
 }
---
+++ src/member.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function Member() {
+    return (
+        <div style={{ textAlign: 'center', padding: '20px' }}>Member Page</div>
+    )
+}
